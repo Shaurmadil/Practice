b45ea0e2c5034969269825a33c81a1cac05b765f
Yarmola Maxim <yarmola258@gmail.com>
2023-05-08 16:07:54 +0300

    Yarmola's practice

---
+++ smartbasket/php/config.php
@@ -0,0 +1,18 @@
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/phpmailer.php');
+
+		// *** SMTP *** //
+
+		 // require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
+		 // const HOST = '';
+		 // const LOGIN = '';
+		 // const PASS = '';
+		 // const PORT = '';
+
+		// *** /SMTP *** //
+   
+    const SENDER = 'yarmolochkin@gmail.com';
+    const CATCHER = 'yarmola258@gmail.com';
+    const SUBJECT = 'ELFFLLOW';
+    const CHARSET = 'UTF-8';
+    
